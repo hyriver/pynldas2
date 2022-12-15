04a5c04acf485ec2daac8224eb46e8f26e0f117a
cheginit <cheginit@gmail.com>
2022-12-15 16:09:58 -0600

    ENH: Add tests for the main functions

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,10 +1,34 @@
 """Tests for exceptions and requests"""
 import pytest
+from shapely.geometry import Polygon
 
 import pynldas2 as nldas
-from pynldas2 import NLDASServiceError
+from pynldas2 import InputRangeError, InputTypeError, InputValueError
 
-def test_invalid_method():
-    with pytest.raises(NLDASServiceError) as ex:
-        _ = nldas.get_bycoords()
-    assert "GET" in str(ex.value)
+GEOM = Polygon(
+    [[-69.77, 45.07], [-69.31, 45.07], [-69.31, 45.45], [-69.77, 45.45], [-69.77, 45.07]]
+)
+VAR = ["prcp", "pet"]
+DEF_CRS = 4326
+ALT_CRS = 3542
+COORDS = (-1431147.7928, 318483.4618)
+START = "2000-01-01"
+END = "2000-01-12"
+
+
+def test_coords_range():
+    with pytest.raises(InputRangeError) as ex:
+        _ = nldas.get_bycoords(COORDS, START, END, DEF_CRS, VAR)
+    assert "range" in str(ex.value)
+
+
+def test_coords_type():
+    with pytest.raises(InputTypeError) as ex:
+        _ = nldas.get_bycoords(COORDS[0], START, END, ALT_CRS, VAR)
+    assert "tuple" in str(ex.value)
+
+
+def test_coords_var():
+    with pytest.raises(InputValueError) as ex:
+        _ = nldas.get_bycoords(COORDS, START, END, ALT_CRS, "tmin")
+    assert "prcp" in str(ex.value)
--- tests/test_pynldas2.py
+++ tests/test_pynldas2.py
@@ -1,7 +1,42 @@
 """Tests for the package PyNLDAS2."""
 import io
 
+import numpy as np
+from shapely.geometry import Polygon
+
 import pynldas2 as nldas
+
+GEOM = Polygon(
+    [[-69.77, 45.07], [-69.31, 45.07], [-69.31, 45.45], [-69.77, 45.45], [-69.77, 45.07]]
+)
+VAR = ["prcp", "pet"]
+DEF_CRS = 4326
+ALT_CRS = 3542
+COORDS = (-1431147.7928, 318483.4618)
+START = "2000-01-01"
+END = "2000-01-12"
+
+
+def assert_close(a: float, b: float, rtol: float = 1e-2) -> bool:
+    assert np.isclose(a, b, rtol=rtol).all()
+
+
+def test_coords():
+    clm = nldas.get_bycoords(COORDS, START, END, ALT_CRS, VAR)
+    assert_close(clm.prcp.mean(), 0.0051)
+    assert_close(clm.pet.mean(), 0.1346)
+
+
+def test_coords_xr():
+    clm = nldas.get_bycoords(COORDS, START, END, ALT_CRS, VAR, True)
+    assert_close(clm.prcp.mean(), 0.0051)
+    assert_close(clm.pet.mean(), 0.1346)
+
+
+def test_geom():
+    clm = nldas.get_bygeom(GEOM, START, END, DEF_CRS, VAR)
+    assert_close(clm.prcp.mean(), 0.1534)
+    assert_close(clm.pet.mean(), 0.0418)
 
 
 def test_show_versions():
