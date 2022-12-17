977283986d35dbf2bbdac38daa821bc2e429e000
cheginit <cheginit@gmail.com>
2022-12-16 18:39:35 -0600

    ENH: Add a CI env var to indicate tests are running in Github Actions and use only 1 parallel connection.

--- tests/test_pynldas2.py
+++ tests/test_pynldas2.py
@@ -1,5 +1,6 @@
 """Tests for the package PyNLDAS2."""
 import io
+import os
 
 import numpy as np
 from shapely.geometry import Polygon
@@ -15,6 +16,7 @@ ALT_CRS = 3542
 COORDS = (-1431147.7928, 318483.4618)
 START = "2000-01-01"
 END = "2000-01-12"
+CONN = 1 if int(os.environ.get("GH_CI", 0)) else 4
 
 
 def assert_close(a: float, b: float, rtol: float = 1e-2) -> bool:
@@ -22,24 +24,24 @@ def assert_close(a: float, b: float, rtol: float = 1e-2) -> bool:
 
 
 def test_coords():
-    clm = nldas.get_bycoords(COORDS, START, END, ALT_CRS, VAR)
+    clm = nldas.get_bycoords(COORDS, START, END, ALT_CRS, VAR, n_conn=CONN)
     assert_close(clm.prcp.mean(), 0.0051)
     assert_close(clm.pet.mean(), 0.1346)
 
 
 def test_coords_xr():
-    clm = nldas.get_bycoords(COORDS, START, END, ALT_CRS, "prcp", True)
+    clm = nldas.get_bycoords(COORDS, START, END, ALT_CRS, "prcp", True, n_conn=CONN)
     assert_close(clm.prcp.mean(), 0.0051)
 
 
 def test_geom():
-    clm = nldas.get_bygeom(GEOM, START, END, DEF_CRS, VAR)
+    clm = nldas.get_bygeom(GEOM, START, END, DEF_CRS, VAR, n_conn=CONN)
     assert_close(clm.prcp.mean(), 0.1534)
     assert_close(clm.pet.mean(), 0.0418)
 
 
 def test_geom_box():
-    clm = nldas.get_bygeom(GEOM.bounds, START, END, DEF_CRS, "prcp")
+    clm = nldas.get_bygeom(GEOM.bounds, START, END, DEF_CRS, "prcp", n_conn=CONN)
     assert_close(clm.prcp.mean(), 0.1534)
 
 
