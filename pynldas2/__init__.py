8b3f0bd7e6a603be3423522015216ab2b83cba0d
Taher Chegini <cheginit@gmail.com>
2024-05-08 18:04:55 -0400

    MNT: Add the exception module as a high-level API so in duture version exceptions will be removed from high-level APIs. [skip ci]

--- pynldas2/__init__.py
+++ pynldas2/__init__.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from importlib.metadata import PackageNotFoundError, version
 
+from pynldas2 import exceptions
 from pynldas2.exceptions import InputRangeError, InputTypeError, InputValueError, NLDASServiceError
 from pynldas2.print_versions import show_versions
 from pynldas2.pynldas2 import get_bycoords, get_bygeom, get_grid_mask
@@ -22,4 +23,6 @@ __all__ = [
     "InputTypeError",
     "InputValueError",
     "NLDASServiceError",
+    "exceptions",
+    "__version__",
 ]
